42698969c0e1ca7cd041020dc058fbf39681929e
kash2Jul <mgmata1@buffs.wtamu.edu>
2016-12-01 00:22:29 +0000

    redo

--- AirplaneService/github.sh
+++ AirplaneService/github.sh
@@ -4,5 +4,5 @@
 git add .
 read -p "Commit description: " desc
 git commit -m "$desc"
-git remote add origin git@github.com:kashJul2214/CIDM4382_Fall2016.git
+git remote add origin git@github.com:kashJul2214/Airplane_Service_CIDM4382_Fall2016.git
 git push -u origin master
